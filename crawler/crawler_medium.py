f3e7092ae0b9995ef508dbc2e0b8eb14c8fdff80
xuefei <10670311954@qq.com>
2024-09-12 07:03:06 +0800

    feat: 添加翻译功能

--- crawler/crawler_medium.py
+++ crawler/crawler_medium.py
@@ -4,6 +4,8 @@ import os
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+from util import translate
+
 from mdutils import MdUtils
 
 def crawler_medium(url):
@@ -32,7 +34,6 @@ def crawler_medium(url):
 
     content_divs = section.find_all("div", class_="fj fk fl fm fn")
     for content_div in content_divs:
-        print(content_div.get("class"))
         extract(content_div,md_file)
     md_file.create_md_file()
 
@@ -46,18 +47,22 @@ def extract(div,md_file):
             if class_arr is not None:
                 class_str = " ".join(class_arr)
             if class_str != "speechify-ignore ab cp":
-                print(child.get("class"))
                 extract(child,md_file)
         if child.name == "h1" or child.name == "h2" or child.name == "h3" or child.name == "h4":
             title = child.string.strip()
+            translated_title = translate.translate(title)
             if child.name == "h1":
                 md_file.new_header(level=1, title=title)
+                md_file.new_header(level=1, title=translated_title)
             if child.name == "h2":
                 md_file.new_header(level=2, title=title)
+                md_file.new_header(level=2, title=translated_title)
             if child.name == "h3":
                 md_file.new_header(level=3, title=title)
+                md_file.new_header(level=3, title=translated_title)
             if child.name == "h4":
                 md_file.new_header(level=4, title=title)
+                md_file.new_header(level=4, title=translated_title)
             print(child.string.strip())
         if child.name == "figure":
             img = child.find("img")
@@ -65,15 +70,17 @@ def extract(div,md_file):
 
             markdownImageStr = md_file.new_inline_image("", img_src)
             md_file.new_line(markdownImageStr)
-            print(img_src)
         if child.name == "p":
             phrase = child.text.strip()
+            translated_phrase = translate.translate(phrase)
             md_file.new_paragraph(phrase)
-            print(phrase)
+            md_file.new_paragraph(translated_phrase)
         if child.name == "ul":
             lis = child.find_all("li")
             li_text_arr = []
             for li in lis:
-                li_text_arr.append(li.text.strip())
-                print(li.text.strip())
+                li_text = li.text.strip()
+                translated_li_text = translate.translate(li_text)
+                li_text_arr.append(li_text)
+                li_text_arr.append(translated_li_text)
             md_file.new_list(li_text_arr)
